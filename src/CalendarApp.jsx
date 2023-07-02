074d8f1c1fb2a6e5d7720313e40dfda628036c8d
Maycol Perez <maycoldanielp@gmail.com>
2023-07-02 18:57:10 -0400

    Feat: HashRouter for spa

--- src/CalendarApp.jsx
+++ src/CalendarApp.jsx
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AppRouter } from './router';
 import { store } from './store';
 import 'animate.css';
@@ -7,9 +7,11 @@ import 'animate.css';
 export const CalendarApp = () => {
    return (
       <Provider store={store}>
-         <BrowserRouter>
+         <HashRouter>
             <AppRouter />
-         </BrowserRouter>
+         </HashRouter>
+         {/* <BrowserRouter>
+         </BrowserRouter> */}
       </Provider>
    );
 };
