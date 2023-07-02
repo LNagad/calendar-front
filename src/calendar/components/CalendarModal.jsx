bf5e22288162f7b2dd2a370be47c11ed4df60b92
Maycol Perez <maycoldanielp@gmail.com>
2023-07-02 18:54:13 -0400

    FIX: updating event title on modal open

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -43,7 +43,7 @@ export const CalendarModal = () => {
          overlayClassName='modal-fondo'
          closeTimeoutMS={ 200 }
       >
-         <h1> Nuevo evento </h1>
+         <h1> {formValues.title || 'Nuevo evento'} </h1>
          <hr />
          <form className="container" onSubmit={ onSubmit }>
 
